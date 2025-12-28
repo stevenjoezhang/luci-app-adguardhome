a22b9b43f8d0635a8684c6f0895390967c226856
Mimi <1119186082@qq.com>
2025-12-28 14:46:11 +0800

    Update messages in update_core.sh

--- root/usr/share/AdGuardHome/update_core.sh
+++ root/usr/share/AdGuardHome/update_core.sh
@@ -10,52 +10,53 @@ upxflag=$(uci get AdGuardHome.AdGuardHome.upxflag 2>/dev/null)
 
 check_if_already_running(){
 	running_tasks="$(ps |grep "AdGuardHome" |grep "update_core" |grep -v "grep" |awk '{print $1}' |wc -l)"
-	[ "${running_tasks}" -gt "2" ] && echo -e "\nA task is already running."  && EXIT 2
+	[ "${running_tasks}" -gt "2" ] && echo -e "\nA task is already running." && EXIT 2
 }
 
 check_wgetcurl(){
+	echo "Checking for curl or wget..."
 	which curl && downloader="curl -L -k --retry 2 --connect-timeout 20 -o" && return
 	which wget-ssl && downloader="wget-ssl --no-check-certificate -t 2 -T 20 -O" && return
-	[ -z "$1" ] && opkg update || (echo error opkg && EXIT 1)
+	[ -z "$1" ] && opkg update || (echo "Failed to run opkg update" && EXIT 1)
 	[ -z "$1" ] && (opkg remove wget wget-nossl --force-depends ; opkg install wget ; check_wgetcurl 1 ;return)
 	[ "$1" == "1" ] && (opkg install curl ; check_wgetcurl 2 ; return)
-	echo error curl and wget && EXIT 1
+	echo "Error: curl and wget not found" && EXIT 1
 }
 check_latest_version(){
 	check_wgetcurl
-	echo -e "Check for update..."
+	echo "Check for update..."
 	latest_ver="$($downloader - https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name'|head -n1|cut -d '"' -f4 2>/dev/null)"
 
 	if [ -z "${latest_ver}" ]; then
-		echo -e "\nFailed to check latest version, please try again later."  && EXIT 1
+		echo "Failed to check latest version, please try again later." && EXIT 1
 	fi
-	now_ver="$($binpath --version 2>/dev/null | grep -m 1 -oE '[v]{0,1}[0-9]+[.][Bbeta0-9\.\-]+')"
-	if [ "${latest_ver}"x != "${now_ver}"x ] || [ "$1" == "force" ]; then
-		echo -e "Local version: ${now_ver}. Cloud version: ${latest_ver}."
+	local_ver="$($binpath --version 2>/dev/null | grep -m 1 -oE '[v]{0,1}[0-9]+[.][Bbeta0-9\.\-]+')"
+	echo "Local version: ${local_ver}. Latest version: ${latest_ver}."
+	if [ "${latest_ver}"x != "${local_ver}"x ] || [ "$1" == "force" ]; then
 		doupdate_core
 	else
-			echo -e "\nLocal version: ${now_ver}, cloud version: ${latest_ver}."
-			echo -e "You're already using the latest version."
-			if [ ! -z "$upxflag" ]; then
-				filesize=$(ls -l $binpath | awk '{ print $5 }')
-				if [ $filesize -gt 8000000 ]; then
-					echo -e "start upx may take a long time"
-					doupx
-					mkdir -p "/tmp/AdGuardHomeupdate/AdGuardHome" >/dev/null 2>&1
-					rm -fr /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/}
-					/tmp/upx-${upx_latest_ver}-${Arch}_linux/upx $upxflag $binpath -o /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/}
-					rm -rf /tmp/upx-${upx_latest_ver}-${Arch}_linux
-					/etc/init.d/AdGuardHome stop nobackup
-					rm $binpath
-					mv -f /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/} $binpath
-					/etc/init.d/AdGuardHome start
-					echo -e "finished"
-				fi
+		echo "You're already using the latest version."
+		if [ ! -z "$upxflag" ]; then
+			filesize=$(ls -l $binpath | awk '{ print $5 }')
+			if [ $filesize -gt 8000000 ]; then
+				doupx
+				mkdir -p "/tmp/AdGuardHomeupdate/AdGuardHome" >/dev/null 2>&1
+				rm -fr /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/}
+				/tmp/upx-${upx_latest_ver}-${Arch}_linux/upx $upxflag $binpath -o /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/}
+				rm -rf /tmp/upx-${upx_latest_ver}-${Arch}_linux
+				/etc/init.d/AdGuardHome stop nobackup
+				rm $binpath
+				mv -f /tmp/AdGuardHomeupdate/AdGuardHome/${binpath##*/} $binpath
+				/etc/init.d/AdGuardHome start
+				echo "Finished"
 			fi
-			EXIT 0
+		fi
+		EXIT 0
 	fi
 }
 doupx(){
+	echo "Start running upx. It may take a long time..."
+
 	um="$(uname -m)"
 
 	case "$um" in
@@ -111,24 +112,24 @@ doupx(){
         ;;
 
     *)
-        echo "error not support $um if you can use official release please issue a bug"
+        echo "Error: $um is not supported"
         exit 1
         ;;
 	esac
 	upx_latest_ver="$($downloader - https://api.github.com/repos/upx/upx/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E '[0-9.]+' -o 2>/dev/null)"
 	$downloader /tmp/upx-${upx_latest_ver}-${Arch}_linux.tar.xz "https://github.com/upx/upx/releases/download/v${upx_latest_ver}/upx-${upx_latest_ver}-${Arch}_linux.tar.xz" 2>&1
 	#tar xvJf
-	which xz || (opkg list | grep ^xz || opkg update && opkg install xz) || (echo "xz download fail" && EXIT 1)
+	which xz || (opkg list | grep ^xz || opkg update && opkg install xz) || (echo "Failed to install xz, it's required for installing upx." && EXIT 1)
 	mkdir -p /tmp/upx-${upx_latest_ver}-${Arch}_linux
 	xz -d -c /tmp/upx-${upx_latest_ver}-${Arch}_linux.tar.xz| tar -x -C "/tmp" >/dev/null 2>&1
 	if [ ! -e "/tmp/upx-${upx_latest_ver}-${Arch}_linux/upx" ]; then
-		echo -e "Failed to download upx."
+		echo "Failed to download upx."
 		EXIT 1
 	fi
 	rm /tmp/upx-${upx_latest_ver}-${Arch}_linux.tar.xz
 }
 doupdate_core(){
-	echo -e "Updating core..."
+	echo "Updating core..."
 	mkdir -p "/tmp/AdGuardHomeupdate"
 	rm -rf /tmp/AdGuardHomeupdate/* >/dev/null 2>&1
 	Arch=$(uci -q get AdGuardHome.AdGuardHome.arch)
@@ -196,12 +197,12 @@ doupdate_core(){
 
         # ------------ Others ----------
         *)
-            echo "error not support $um, if you can use official release please file a bug"
+            echo "Error: $um is not supported"
             exit 1
             ;;
     esac
 	fi
-	echo -e "start download"
+	echo "Start download..."
 	downloadlinks=$(uci get AdGuardHome.AdGuardHome.downloadlinks 2>/dev/null)
 	if [ -z "$downloadlinks" ]; then
 		echo "No download links configured in UCI"
@@ -212,9 +213,10 @@ doupdate_core(){
 	do
 		[ -n "$link" ] || continue
 		eval link="$link"
+		echo "Trying to download from: $link"
 		$downloader /tmp/AdGuardHomeupdate/${link##*/} "$link" 2>&1
 		if [ "$?" != "0" ]; then
-			echo "download failed try another download"
+			echo "Download failed. Trying next link..."
 			rm -f /tmp/AdGuardHomeupdate/${link##*/}
 		else
 			local success="1"
@@ -222,11 +224,11 @@ doupdate_core(){
 		fi
 	done < "/tmp/AdG_links.txt"
 	rm /tmp/AdG_links.txt
-	[ -z "$success" ] && echo "no download success" && EXIT 1
+	[ -z "$success" ] && echo "All downloads failed." && EXIT 1
 	if [ "${link##*.}" == "gz" ]; then
 		tar -zxf "/tmp/AdGuardHomeupdate/${link##*/}" -C "/tmp/AdGuardHomeupdate/"
 		if [ ! -e "/tmp/AdGuardHomeupdate/AdGuardHome" ]; then
-			echo -e "Failed to download core."
+			echo "Failed to download core."
 			rm -rf "/tmp/AdGuardHomeupdate" >/dev/null 2>&1
 			EXIT 1
 		fi
@@ -235,25 +237,23 @@ doupdate_core(){
 		downloadbin="/tmp/AdGuardHomeupdate/${link##*/}"
 	fi
 	chmod 755 $downloadbin
-	echo -e "download success start copy"
+	echo "Download successful."
 	if [ -n "$upxflag" ]; then
-		echo -e "start upx may take a long time"
 		doupx
 		/tmp/upx-${upx_latest_ver}-${Arch}_linux/upx $upxflag $downloadbin
 		rm -rf /tmp/upx-${upx_latest_ver}-${Arch}_linux
 	fi
-	echo -e "start copy"
+	echo "Start copy to ${binpath}"
 	/etc/init.d/AdGuardHome stop nobackup
 	rm -f "$binpath"
 	mv -f "$downloadbin" "$binpath"
 	if [ "$?" == "1" ]; then
-		echo "mv failed maybe not enough space please use upx or change bin to /tmp/AdGuardHome"
+		echo "Error: mv failed. Maybe not enough space. Please use upx or change bin path to /tmp/AdGuardHome."
 		EXIT 1
 	fi
 	/etc/init.d/AdGuardHome start
 	rm -rf "/tmp/AdGuardHomeupdate" >/dev/null 2>&1
-	echo -e "Succeeded in updating core."
-	echo -e "Local version: ${latest_ver}, cloud version: ${latest_ver}.\n"
+	echo -e "Core updated successfully. New version: ${latest_ver}.\n"
 	EXIT 0
 }
 EXIT(){
@@ -262,7 +262,7 @@ EXIT(){
 	exit $1
 }
 main(){
-	
+
 	check_if_already_running
 	check_latest_version $1
 }
